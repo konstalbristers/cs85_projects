2e7f6c005d517a37f796043b046dd1e3c9e7eef9
konstalbristers <kcparis33@gmail.com>
2025-07-05 17:31:06 -0700

    Added if statement for size XL and its details

--- module2a/price_engine.php
+++ module2a/price_engine.php
@@ -34,7 +34,11 @@
             // }
             if ($size === "L") {
                 $finalPrice = $finalPrice + 1.75;
-                $details .= "<li>Size (L) Upcharge: <span>+$1.75</span></li>";
+                $details .= "<li>Size (L) Upcharge: <span>+ $1.75</span></li>";
+            }
+            if ($size === "XL") {
+                $finalPrice = $finalPrice + 2.50;
+                $details .= "<li>Size (XL) Upcharge: <span>+ $2.50</span></li>";
             }
 
 
